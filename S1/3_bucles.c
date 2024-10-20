9d6c67f39702092a457b9230a19a2c88d991e6c5
Álvaro <mailproyectos04@gmail.com>
2024-10-20 07:15:07 -0700

    Semana 1

--- S1/3_bucles.c
+++ S1/3_bucles.c
@@ -1,19 +1,28 @@
 #include <stdio.h>
 
 /*
- * Que es una array? 
+ * Como funciona un bucle while? 
  *
  * Para que sirven?
  * 
- * Que es una String?
- * 
- * Arrays multidimensionales???
 */
 
 int main()
 {
+    while (/* condition */0)
+    {
+        /* code */
+    }
 
+    // crea un codigo que sume 1 a los elementos del array
+    int array[11] = {-1, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+    int i = 0;
 
+    while(i < 11)
+    {
+        array[i] = //????
+        i++; // Es lo mismo que i = i + 1;
+    }
 
     return 0;
 }
--- S1/_ejer_repaso.c
+++ S1/_ejer_repaso.c
@@ -4,9 +4,30 @@
 int main()
 {
 
+// Primero imprime "Hola mundo!"
+
+// despues escribe tu nombre en ascii, por ejemplo
+    char Ej[7];
+    Ej[0] = 65;
+    Ej[1] = 108;
+    Ej[2] = 118;
+    Ej[3] = 97;
+    Ej[4] = 114;
+    Ej[5] = 111;
+    Ej[6] = 0;
+
+    printf("%s\n", Ej);
+
+    // Ahora tu:
+    char Alejandro[10];
 
 
 
 
+
+
+    Alejandro[9] = 0; // Siempre que se ponen muchos char's seguidos se acaba con un 0.
+
+    printf("%s\n", Alejandro);
     return 0;
 }
